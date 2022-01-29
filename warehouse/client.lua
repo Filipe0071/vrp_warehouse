ad4e165c25d4e81ed0f303e7abe75d63b49c7616
warfa <63048792+warfas@users.noreply.github.com>
2022-01-29 19:28:20 +0200

    Update client.lua more locations

--- warehouse/client.lua
+++ warehouse/client.lua
@@ -1,57 +1,49 @@
 local inWarehouse, inMission = false, false
 Citizen.CreateThread(function()
-    local joinLoc, leaveLoc, computerLoc = vec3(-145.19108581543,-1429.9975585938,30.911861419678), vec3(1087.7553710938,-3099.4028320312,-38.999950408936), vec3(1088.3489990234,-3101.2429199219,-38.999961853027)
     local threads = 1000
     while true do
         local player = PlayerPedId()
         local playerCoords = GetEntityCoords(player)
 
-        -- JOIN & LEAVE (WAREHOUSE)
-        if joinLoc then
-            local x,y,z = joinLoc.x, joinLoc.y, joinLoc.z
+        -- cam twisted codu asta, plm
+        for i,v in pairs(config) do
+            
             if not inWarehouse then
+                local x,y,z = v.join[1], v.join[2], v.join[3]
                 if #(vec3(x,y,z) - playerCoords) < 2.5 then
-                    DrawText3D(x,y,z + 0.45, "~w~Warehouse ~g~Behind Shop", 0.75, 0)
+                    DrawText3D(x,y,z + 0.45, "~w~Warehouse ~g~"..v.name, 0.75, 0)
                     if #(vec3(x,y,z) - playerCoords) < 0.5 then
                         DrawText3D(x,y,z + 0.30, "Apasa ~g~E ~w~pentru a intra in ~g~Warehouse", 0.65, 0)
-                        DrawText3D(x,y,z + 0.15, "Proprietar: ~g~warfa", 0.65, 0)
                         if IsControlJustPressed(0, 51) then inWarehouse = true
-                            SetEntityCoords(player, leaveLoc.x, leaveLoc.y, leaveLoc.z)
+                            SetEntityCoords(player, v.leave[1], v.leave[2], v.leave[3])
                         end
                     end
-                    threads = 0
+                    threads = 0 
                 else
                     threads = 500
                 end
             end
-        end
-
-        if leaveLoc then
-            local x,y,z = leaveLoc.x, leaveLoc.y, leaveLoc.z
+            
             if inWarehouse then
+                local x,y,z = v.leave[1], v.leave[2], v.leave[3]
+                local x2,y2,z2 = v.computer[1], v.computer[2], v.computer[3]
+
                 if #(vec3(x,y,z) - playerCoords) < 1 then
-                    DrawText3D(x,y,z + 0.45, "~w~Warehouse ~g~Behind Shop", 0.75, 0)
+                    DrawText3D(x,y,z + 0.45, "~w~Warehouse ~g~"..v.name, 0.75, 0)
                     if #(vec3(x,y,z) - playerCoords) < 0.5 then
                         DrawText3D(x,y,z + 0.30, "Apasa ~g~E ~w~pentru a iesi din ~g~Warehouse", 0.65, 0)
                         if IsControlJustPressed(0, 51) then inWarehouse = false
-                            SetEntityCoords(player, joinLoc.x, joinLoc.y, joinLoc.z)
+                            SetEntityCoords(player, v.join[1], v.join[2], v.join[3])
                         end
                     end
                 else
                     threads = 500
                 end
-                threads = 0
-            end
-        end
 
-        -- ACCES COMPUTER
-        if computerLoc then
-            local x,y,z = computerLoc.x, computerLoc.y, computerLoc.z
-            if inWarehouse then
-                if #(vec3(x,y,z) - playerCoords) < 1 then
-                    DrawText3D(x,y,z + 0.45, "~w~Calculator de ~g~Misiuni", 0.75, 0)
-                    if #(vec3(x,y,z) - playerCoords) < 0.5 then
-                        DrawText3D(x,y,z + 0.30, "Apasa ~g~E ~w~pentru deschide ~g~Calculator", 0.65, 0)
+                if #(vec3(x2,y2,z2) - playerCoords) < 1 then
+                    DrawText3D(x2,y2,z2 + 0.45, "~w~Laptop ~g~"..v.name, 0.75, 0)
+                    if #(vec3(x2,y2,z2) - playerCoords) < 0.5 then
+                        DrawText3D(x2,y2,z2 + 0.30, "Apasa ~g~E ~w~a deschide Laptop ~g~"..v.name, 0.65, 0)
                         if IsControlJustPressed(0, 51) then
                             TriggerServerEvent("warehouse:openMenu", inWarehouse)
                         end
@@ -59,8 +51,10 @@ Citizen.CreateThread(function()
                 else
                     threads = 500
                 end
+
                 threads = 0
             end
+
         end
 
         Citizen.Wait(threads)
@@ -69,6 +63,8 @@ end)
 
 RegisterNetEvent("warehouse:firstMission")
 AddEventHandler("warehouse:firstMission", function()
+    local player = PlayerPedId()
+    -- to be continued
 end)
 
 function DrawText3D(x,y,z, text, scl, font) 
