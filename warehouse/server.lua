468fb76926db1689b6a6d9bcb338fd33e6c8ca76
warfa <63048792+warfas@users.noreply.github.com>
2022-01-30 10:56:18 +0200

    Update server.lua (nu e terminat)

--- warehouse/server.lua
+++ warehouse/server.lua
@@ -4,31 +4,28 @@ local Proxy <const> = module("lib/Proxy")
 local vRP <const> = Proxy.getInterface("vRP")
 local vRPclient <const> = Tunnel.getInterface("vRP", "warehouse")
 
-local function openMenu(source)
+RegisterNetEvent("warehouse:checkWarehouse")
+AddEventHandler("warehouse:checkWarehouse", function(warehouse)
     local player = source
     local user_id = vRP.getUserId({player})
-    if user_id ~= nil then
-        local menu = {}
-        menu["#1 MISSION"] = {function(player, choice)
-            TriggerClientEvent("warehouse:firstMission", player)
-            vRPclient.notify(player,{"~r~Iesi din casa pentru a incepe Misiunea"})
-            vRP.closeMenu({player})
-        end, "andrei"}
-        vRP.openMenu({player, menu})
-    end
-end
-
-RegisterNetEvent("warehouse:openMenu")
-AddEventHandler("warehouse:openMenu", function(inWarehouse)
-    local player = source
-    local user_id = vRP.getUserId({player})
-    if user_id ~= nil then
-        if inWarehouse then
-            openMenu(player)          
+    if user_id ~= nil then 
+        local rows = exports.ghmattimysql:executeSync("SELECT id FROM vrp_warehouse WHERE warehouse = @warehouse", {["@warehouse"] = warehouse})[1]
+        if rows then
+            if tonumber(rows.id) == user_id then
+                print(user_id)
+            else
+                vRPclient.notify(player,{"~r~Acest warehouse este deja luat!"})
+            end
         else
-            vRPclient.notify(player,{"~r~Nu esti in warehouse!"})
+            for i,v in pairs(config) do
+                local checkMoney = vRP.getMoney({user_id})
+                if checkMoney > tonumber(v.priceWarehouse) then
+                    exports.ghmattimysql:execute("INSERT INTO vrp_warehouse (warehouse, id) VALUES (@warehouse, @id)", {["@warehouse"] = warehouse, ["@id"] = user_id})
+                    print("cu succes")
+                else
+                    print("n-ai bani fmm")
+                end
+            end
         end
-    else
-        DropPlayer(player, "no id bro")
     end
 end)
